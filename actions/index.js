067526a72fffef6e7d65c4b540951631cf422377
Ricardo Tosin <tosinricardo@gmail.com>
2019-05-28 01:04:29 -0300

    add questions rdx

--- actions/index.js
+++ actions/index.js
@@ -2,15 +2,18 @@ export const GET_DECKS = 'GET_DECKS';
 export const ADD_DECK = 'ADD_DECK';
 export const ADD_QUESTION = 'ADD_QUESTION';
 
+
 export const getDecks = decks => ({
     type: GET_DECKS,
     decks,
 });
 
+
 export const addDeck = deck => ({
     type: ADD_DECK,
     deck,
 });
+
 
 export const addQuestion = card => ({
     type: ADD_QUESTION,
